ca369b8f7eb0ccd6e2975714eafdab88aa7140fe
Sahire <sahira.mammadova.653.19@gmail.com>
2022-05-04 13:21:44 +0400

    project

---
+++ js/forResponsive.js
@@ -0,0 +1,31 @@
+
+let menu=document.querySelector('.menu')
+let clonedMenu=menu.cloneNode(true);
+let nav=document.querySelector('nav');
+nav.appendChild(clonedMenu);
+clonedMenu.style.display="none"
+clonedMenu.classList.add('side-menu');
+let menuLogo=document.querySelector('.menu-logo');
+const appear=()=>{
+  clonedMenu.style.display="block";
+  let remove=document.createElement('i');
+  let firstli=clonedMenu.querySelector('li:first-child');
+  clonedMenu.insertBefore(remove,firstli);
+  remove.classList.add('fa-solid','fa-xmark');
+  remove.style.position="absolute";
+  remove.style.top="20px"
+  remove.style.right="20px";
+  remove.style.cursor="pointer"
+  remove.addEventListener("click",()=>{
+    clonedMenu.style.display="none"
+  })
+}
+menuLogo.addEventListener("click",appear);
+
+let allLi=clonedMenu.querySelectorAll('li');
+
+allLi.forEach((item)=>{
+  item.addEventListener('click',()=>{
+    clonedMenu.style.display="none"
+  })
+})
